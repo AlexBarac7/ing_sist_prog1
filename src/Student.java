feac01675744f952d65cdcb20adae585860ed096
AlexBarac7 <alex.barack77@gmail.com>
2026-03-09 19:40:27 +0200

    Laborator 1

---
+++ src/Student.java
@@ -0,0 +1,15 @@
+public class Student {
+    int nrMatricol;
+    String prenume, nume;
+    String formatieDeStudiu;
+    public Student(int nrMatricol, String prenume, String nume, String formatieDeStudiu) {
+        this.nrMatricol = nrMatricol;
+        this.prenume = prenume;
+        this.nume = nume;
+        this.formatieDeStudiu = formatieDeStudiu;
+    }
+    @Override
+    public String toString() {
+        return prenume +  " " + nume + "\n" + "Grupa: " + formatieDeStudiu + "\n" +  "Nr matricol: " + nrMatricol;
+    }
+}
